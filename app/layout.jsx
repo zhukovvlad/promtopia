4a45279760325bbe7a17d2af607b8466defa5c95
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-06-22 19:17:48 +0300

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Ptromtopia",
+  description: "Discover and share AI promts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
