c5fd8025fb8256c4d875c27938cfc31f756ebc4c
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-06-28 14:52:20 +0300

    Implemented handleCopy for copy prompt in clipboard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,6 +8,12 @@ import { usePathname, useRouter } from "next/navigation";
 const PromtCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
 
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 3000);
+  };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -28,7 +34,7 @@ const PromtCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             </p>
           </div>
         </div>
-        <div className="copy_btn" onClick={() => {}}>
+        <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
               copied === post.prompt
@@ -40,6 +46,13 @@ const PromtCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           />
         </div>
       </div>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
     </div>
   );
 };
